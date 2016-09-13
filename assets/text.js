2f6202adac8fa8ede880439517bb833d291e8e21
Daniel St. Clair <stclair.daniel@gmail.com>
2016-09-13 16:13:18 -0700

    changed text loading to assets, added space chr

---
+++ assets/text.js
@@ -0,0 +1,4 @@
+const textArray = [
+"I oo OO textI oo OO textI oo OO textI oo OO textI oo OO textI oo OO textI am OO textI am OO textI am some textI am some textI am some textI am some textI am some textI am some textI am some textI am some textI am some text"];
+
+export default textArray;
